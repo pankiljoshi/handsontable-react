784e8e32c111b6120a19bd3ce6a56608c553c220
pankiljoshi <pankil@zeroek.in>
2024-09-10 22:17:06 +0530

    adding formulas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Handsontable from 'handsontable';
 import { HotTable } from '@handsontable/react';
+import HyperFormula from 'hyperformula';
 
 function getDebugInfo() {
   let debug = 'Handsontable:';
@@ -18,8 +19,10 @@ function App() {
     ["", "Ford", "Volvo", "Toyota", "Honda"],
     ["2016", 10, 11, 12, 13],
     ["2017", 20, 11, 14, 13],
-    ["2018", 30, 15, 12, 13]
-  ]
+    ["2018", 30, 15, 12, 13],
+    ["Total", "=SUM(B2:B4)", "=SUM(C2:C4)", "=SUM(D2:D4)", "=SUM(E2:E4)"],
+    ["Average", "=AVERAGE(B2:B4)", "=AVERAGE(C2:C4)", "=AVERAGE(D2:D4)", "=AVERAGE(E2:E4)"]
+  ];
 
   return (
     <div id="example">
@@ -29,15 +32,18 @@ function App() {
         height="100%"
         colHeaders={true}
         rowHeaders={true}
-        contextMenu={true} 
-        manualRowMove={true} 
+        contextMenu={true}
+        manualRowMove={true}
         bindRowsWithHeaders="strict"
         filters={true}
-        manualColumnMove={true} 
-        autoWrapRow={true} 
+        manualColumnMove={true}
+        autoWrapRow={true}
         autoWrapCol={true}
         dropdownMenu={true}
         fillHandle={true}
+        formulas={{
+          engine: HyperFormula,
+        }}
         licenseKey="non-commercial-and-evaluation"
       />
     </div>
